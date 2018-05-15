f336e695b4628d855fe088cc819b2d09c46ba2e9
John <403778642@qq.com>
2018-05-15 10:36:16 +0800

    提交

---
+++ test/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DateTime: 2018/5/15 9:43
+ * Author: John
+ * Email: 2639816206@qq.com
+ */
+
+// Autoload files using Composer autoload
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+use Request\SignatureHelper;
+//$help = new \SignatureHelper();
+
+SignatureHelper::ceshi();
+
+
+
